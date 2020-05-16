819a412a44c4be87986740ab2535f93c615e650c
Jauneyel <nabijonx@mail.ru>
2020-05-16 16:14:45 +0500

    add resp

---
+++ script.js
@@ -0,0 +1,20 @@
+let name = 'Василий';
+let admin = name;
+alert (admin);
+
+let a = 1000;
+let b = '108';
+alert (a + b);
+
+let x = +prompt('Значения должны быть любые числа 1');
+let y = +prompt('Значения должны быть любые числа 2');
+alert (x + y);
+alert (x - y);
+alert (x * y);
+alert (x / y);
+
+let X = +prompt ('Значение должны быть любые числа');
+let Y = +prompt ('Значение должны быть любые числа');
+let Z = +prompt ('Значение должны быть любые числа');
+let res =((X + Y + Z)/3);
+alert (res);
